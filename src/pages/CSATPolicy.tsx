89e3e45bb40b574c3cd65a031a26be33774e640f
Palash Nath <palashnath880@gmail.com>
2024-06-21 14:10:16 +0600

    Policy PDF Viewer Issue Fixed

--- src/pages/CSATPolicy.tsx
+++ src/pages/CSATPolicy.tsx
@@ -1,15 +1,43 @@
+import { useEffect, useState } from "react";
 import PageHeader from "../components/shared/PageHeader";
 import { useAppSelector } from "../hooks";
+import { Worker } from "@react-pdf-viewer/core";
+import { Viewer } from "@react-pdf-viewer/core";
+import "@react-pdf-viewer/core/lib/styles/index.css";
+import Loader from "../components/shared/Loader";
 
 export default function CSATPolicy() {
+  // states
+  const [pdf, setPdf] = useState("");
+  const [loading, setLoading] = useState<boolean>(true);
+
   // react - redux
   const policyPdfUrl = useAppSelector((state) => state.inventory.policyPdfUrl);
+
+  useEffect(() => {
+    if (policyPdfUrl) {
+      (async () => {
+        const res = await fetch(policyPdfUrl);
+        const data = await res.blob();
+        const objURL = URL.createObjectURL(data);
+        setPdf(objURL);
+        setLoading(false);
+      })();
+    }
+  }, [policyPdfUrl]);
 
   return (
     <div className="">
       <PageHeader title="CSAT Policy" />
-      <div className="h-[90vh]">
-        <iframe src={policyPdfUrl} width={"100%"} height={"100%"}></iframe>
+
+      {loading && <Loader dataLoading />}
+
+      <div className="">
+        {pdf && (
+          <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+            <Viewer fileUrl={pdf} />
+          </Worker>
+        )}
       </div>
     </div>
   );
