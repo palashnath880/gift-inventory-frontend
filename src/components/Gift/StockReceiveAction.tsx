eed50225e32cd5fe218880dbb7cb311c11e9f807
Palash Nath <palashnath880@gmail.com>
2024-07-07 21:17:39 +0600

    Stock Reject Reason Updated

--- src/components/Gift/StockReceiveAction.tsx
+++ src/components/Gift/StockReceiveAction.tsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import { StockType } from "../../types";
-import { Button } from "@mui/material";
+import { Button, Dialog, Divider, TextField, Typography } from "@mui/material";
 import { Close, Done } from "@mui/icons-material";
 import toast from "react-hot-toast";
 import { stockApi } from "../../api/stock";
+import PopupState, { bindDialog, bindTrigger } from "material-ui-popup-state";
 
 export default function StockReceiveAction({
   refetch,
@@ -14,15 +15,21 @@ export default function StockReceiveAction({
 }) {
   // states
   const [loading, setLoading] = useState<boolean>(false);
+  const [reason, setReason] = useState<string>("");
 
   // status update handler
-  const actionHandler = async (status: "rejected" | "received") => {
+  const actionHandler = async (
+    status: "rejected" | "received",
+    close?: () => void
+  ) => {
     try {
       setLoading(true);
-      await stockApi.stockStatusUpdate(stock.id, { status });
+      await stockApi.stockStatusUpdate(stock.id, { status, reason });
       if (status === "received") {
         toast.success("Stock receive successfully.");
       } else {
+        setReason("");
+        typeof close === "function" && close();
         toast.success("Stock rejected.");
       }
 
@@ -34,7 +41,7 @@ export default function StockReceiveAction({
   };
 
   if (stock.status !== "open") {
-    return "";
+    return <></>;
   }
 
   return (
@@ -49,16 +56,64 @@ export default function StockReceiveAction({
       >
         Accept
       </Button>
-      <Button
-        variant="contained"
-        className="!py-2.5 !px-6 !text-sm !capitalize"
-        color="error"
-        startIcon={<Close />}
-        disabled={loading}
-        onClick={() => actionHandler("rejected")}
-      >
-        Reject
-      </Button>
+      <PopupState variant="popover" popupId="rejectStock">
+        {(popupState) => (
+          <>
+            <Button
+              variant="contained"
+              className="!py-2.5 !px-6 !text-sm !capitalize"
+              color="error"
+              startIcon={<Close />}
+              {...bindTrigger(popupState)}
+            >
+              Reject
+            </Button>
+
+            <Dialog {...bindDialog(popupState)}>
+              <div className="sm:w-[450px] px-5 py-4">
+                <Typography
+                  variant="h6"
+                  className="!font-semibold !text-primary"
+                >
+                  Reject Reason
+                </Typography>
+                <Divider className="!my-2" />
+                <div className="mb-5 mt-5">
+                  <TextField
+                    fullWidth
+                    multiline
+                    minRows={3}
+                    label="Reject Reason"
+                    value={reason}
+                    onChange={(e) => setReason(e.target.value)}
+                  />
+                </div>
+                <div className="flex justify-between gap-3">
+                  <Button
+                    variant="contained"
+                    className="!py-2.5 !px-6 !text-sm !capitalize flex-1"
+                    color="error"
+                    disabled={loading || !reason}
+                    onClick={() => actionHandler("rejected", popupState.close)}
+                  >
+                    Reject
+                  </Button>
+                  <Button
+                    variant="outlined"
+                    className="!py-2.5 !px-6 !text-sm !capitalize flex-1"
+                    onClick={() => {
+                      popupState.close();
+                      setReason("");
+                    }}
+                  >
+                    Cancel
+                  </Button>
+                </div>
+              </div>
+            </Dialog>
+          </>
+        )}
+      </PopupState>
     </span>
   );
 }
