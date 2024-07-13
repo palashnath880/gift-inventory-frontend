643b7ba6759fb51e6ac758fa3403e7a14b952826
Palash Nath <palashnath880@gmail.com>
2024-07-13 19:28:48 +0600

    Refresh Button Added

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -1,9 +1,12 @@
-import { Chip, Typography } from "@mui/material";
-import { useAppSelector } from "../../hooks";
+import { Chip, IconButton, Tooltip, Typography } from "@mui/material";
+import { useAppDispatch, useAppSelector } from "../../hooks";
+import { Refresh } from "@mui/icons-material";
+import { loadUser } from "../../features/auth/authSlice";
 
 export default function Topbar() {
   // react-redux
   const user = useAppSelector((state) => state.auth.user);
+  const dispatch = useAppDispatch();
 
   return (
     <div className="bg-white py-3 shadow-md flex justify-between px-5">
@@ -11,6 +14,11 @@ export default function Topbar() {
         <strong>CSC:</strong> {user?.branch}
       </Typography>
       <div className="flex items-center gap-3">
+        <Tooltip title="Refresh Dashboard">
+          <IconButton onClick={() => dispatch(loadUser())}>
+            <Refresh />
+          </IconButton>
+        </Tooltip>
         <Typography variant="h6" className="!font-medium !text-primary">
           <strong>Hello,</strong> {user?.name}
         </Typography>
