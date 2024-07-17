a3de9cf6d70a218bcd0f56955da7463fc1aec754
Palash Nath <palashnath880@gmail.com>
2024-07-17 19:26:02 +0600

    Allocate Item Cancel Reason Dialog Added

--- src/components/Allocate/ActionButtons.tsx
+++ src/components/Allocate/ActionButtons.tsx
@@ -1,6 +1,13 @@
-import { Close, Done } from "@mui/icons-material";
-import { Button, Chip, Popover, Typography } from "@mui/material";
-import PopupState, { bindPopover, bindTrigger } from "material-ui-popup-state";
+import { Close } from "@mui/icons-material";
+import {
+  Button,
+  Chip,
+  Dialog,
+  IconButton,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { bindTrigger } from "material-ui-popup-state";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { allocateApi } from "../../api/allocate";
@@ -8,6 +15,7 @@ import { Link } from "react-router-dom";
 import { verifyVoucherCode } from "../../utility/utility";
 import type { AllocatedItem } from "../../types";
 import { useAppSelector } from "../../hooks";
+import { bindDialog, usePopupState } from "material-ui-popup-state/hooks";
 
 export default function ActionButtons({
   item,
@@ -20,6 +28,13 @@ export default function ActionButtons({
 }) {
   // states
   const [loading, setLoading] = useState<boolean>(false);
+  const [reason, setReason] = useState<string>("");
+
+  // cancel dialog
+  const cancelPopup = usePopupState({
+    variant: "popover",
+    popupId: "cancelPopup",
+  });
 
   // voucher code
   const voucherCodes = useAppSelector((state) => state.inventory.voucherCodes);
@@ -31,9 +46,11 @@ export default function ActionButtons({
   const cancelHandler = async () => {
     try {
       setLoading(true);
-      await allocateApi.redeemItem(item?.id, { type: "cancel" });
+      await allocateApi.redeemItem(item?.id, { type: "cancel", reason });
       refetch();
       toast.success("Allocated item canceled successfully");
+      // close dialog popup
+      typeof cancelPopup.close === "function" && cancelPopup.close();
     } catch (err) {
       toast.error("Sorry! Something went wrong");
     } finally {
@@ -57,52 +74,50 @@ export default function ActionButtons({
         Redeem
       </Button>
 
-      {/* cancel */}
-      <PopupState variant="popover">
-        {(popupState) => (
-          <>
-            <Button
-              variant="contained"
-              color="error"
-              {...bindTrigger(popupState)}
-              disabled={loading}
-              className="!px-6 !py-2.5 !text-sm !capitalize"
-            >
-              Cancel
-            </Button>
-            <Popover {...bindPopover(popupState)}>
-              <div className="w-[220px] bg-white px-3 py-3">
-                <Typography className="!text-base !font-medium !text-center !text-primary">
-                  Are you sure to cancel this item?
-                </Typography>
-                <div className="flex justify-between !gap-2 mt-2">
-                  <Button
-                    variant="contained"
-                    className="!px-6 !py-2.5 !text-sm !capitalize !flex-1"
-                    color="success"
-                    startIcon={<Done />}
-                    onClick={() => {
-                      popupState.close();
-                      cancelHandler();
-                    }}
-                  >
-                    Yes
-                  </Button>
-                  <Button
-                    variant="contained"
-                    className="!px-6 !py-2.5 !text-sm !capitalize !flex-1"
-                    color="error"
-                    startIcon={<Close />}
-                    onClick={popupState.close}
-                  >
-                    No
-                  </Button>
-                </div>
-              </div>
-            </Popover>
-          </>
-        )}
-      </PopupState>
+      {/* cancel button  */}
+      <Button
+        variant="contained"
+        color="error"
+        {...bindTrigger(cancelPopup)}
+        disabled={loading}
+        className="!px-6 !py-2.5 !text-sm !capitalize"
+      >
+        Cancel
+      </Button>
+
+      {/* cancel reason dialog */}
+      <Dialog {...bindDialog(cancelPopup)}>
+        <div className="pt-3 pb-5 max-sm:w-[95%] sm:w-[420px]">
+          <div className="px-5 flex justify-between items-center gap-4 border-b border-primary pb-2">
+            <Typography variant="h6" className="!font-bold !text-primary">
+              Cancel Reason
+            </Typography>
+            <IconButton onClick={cancelPopup.close}>
+              <Close />
+            </IconButton>
+          </div>
+          <div className="px-5 pt-4">
+            <div className="flex flex-col gap-5">
+              <TextField
+                label="Cancel Reason"
+                fullWidth
+                value={reason}
+                onChange={(e) => setReason(e.target.value)}
+              />
+              <Button
+                fullWidth
+                variant="contained"
+                color="error"
+                className="!py-2.5 !text-sm !capitalize"
+                disabled={loading || !reason}
+                onClick={cancelHandler}
+              >
+                Cancel
+              </Button>
+            </div>
+          </div>
+        </div>
+      </Dialog>
     </span>
   );
 }
